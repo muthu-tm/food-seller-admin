a6e159bf8a570063d6849bb1e4c1fd72022bb0d0
Muthu Thavamani <muthucs@outlook.com>
2020-09-26 17:00:04 +0530

    minor fixes

--- lib/screens/products/ProductDetailsScreen.dart
+++ lib/screens/products/ProductDetailsScreen.dart
@@ -22,157 +22,146 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: CustomColors.sellerGreen,
-      appBar: buildAppBar(context),
-      body: Body(product: widget.product),
-    );
-  }
-
-  AppBar buildAppBar(BuildContext context) {
-    return AppBar(
-      backgroundColor: CustomColors.sellerGreen,
-      elevation: 0,
-      leading: IconButton(
-        icon: Icon(Icons.arrow_back),
-        onPressed: () => Navigator.pop(context),
-      ),
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.search),
-          onPressed: () {},
+      appBar: AppBar(
+        backgroundColor: CustomColors.sellerGreen,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
         ),
-      ],
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: getBody(context, widget.product),
     );
   }
-}
 
-class Body extends StatelessWidget {
-  final Products product;
-
-  const Body({Key key, this.product}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
+  Widget getBody(BuildContext context, Products product) {
     Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
-          SizedBox(
-            height: size.height,
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(top: size.height * 0.3),
-                  padding: EdgeInsets.only(
-                    top: size.height * 0.12,
-                    left: 15,
-                    right: 15,
+          Stack(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: size.height * 0.3),
+                padding: EdgeInsets.only(
+                  top: 15,
+                  left: 15,
+                  right: 15,
+                ),
+                decoration: BoxDecoration(
+                  color: CustomColors.sellerLightGrey,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
                   ),
-                  decoration: BoxDecoration(
-                    color: CustomColors.sellerLightGrey,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                    ),
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.symmetric(vertical: 50),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      child: Container(
+                        height: 200,
                         child: Text(
                           product.shortDetails,
                           style: TextStyle(height: 1.5),
                         ),
                       ),
-                      SizedBox(height: 5),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          CartCounter(),
-                          Container(
-                            height: 32,
-                            width: 32,
-                            child: Icon(
-                              Icons.favorite_border,
-                              size: 35,
-                            ),
-                          )
-                        ],
-                      ),
-                      SizedBox(height: 5),
-                      AddToCart(product: product)
-                    ],
-                  ),
+                    ),
+                    SizedBox(height: 5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        CartCounter(),
+                        Container(
+                          height: 32,
+                          width: 32,
+                          child: Icon(
+                            Icons.favorite_border,
+                            size: 35,
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 5),
+                    AddToCart(product: product)
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 50),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        product.name,
-                        style: Theme.of(context)
-                                      .textTheme
-                                      .headline6
-                                      .copyWith(
-                                          color: Colors.white),
-                      ),
-                      SizedBox(height: 50),
-                      Row(
-                        children: <Widget>[
-                          RichText(
-                            text: TextSpan(
-                              children: [
-                                TextSpan(text: "Price\n"),
-                                TextSpan(
-                                  text: "${product.currentPrice}",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .headline4
-                                      .copyWith(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      product.name,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline6
+                          .copyWith(color: Colors.white),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: <Widget>[
+                        RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(text: "Price\n"),
+                              TextSpan(
+                                text: "${product.currentPrice}",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline4
+                                    .copyWith(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: Hero(
+                            tag: "${product.uuid}",
+                            child: CachedNetworkImage(
+                              imageUrl: product.getProductImage(),
+                              imageBuilder: (context, imageProvider) =>
+                                  Container(
+                                width: 150,
+                                height: 125,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10.0),
+                                  ),
+                                  shape: BoxShape.rectangle,
+                                  image: DecorationImage(
+                                      fit: BoxFit.fill, image: imageProvider),
                                 ),
-                              ],
+                              ),
+                              progressIndicatorBuilder:
+                                  (context, url, downloadProgress) =>
+                                      CircularProgressIndicator(
+                                          value: downloadProgress.progress),
+                              errorWidget: (context, url, error) => Icon(
+                                Icons.error,
+                                size: 35,
+                              ),
+                              fadeOutDuration: Duration(seconds: 1),
+                              fadeInDuration: Duration(seconds: 2),
                             ),
                           ),
-                          SizedBox(width: 10),
-                          Expanded(
-                            child: Hero(
-                              tag: "${product.uuid}",
-                              child: CachedNetworkImage(
-                                imageUrl: product.getProductImage(),
-                                imageBuilder: (context, imageProvider) =>
-                                    Container(
-                                  width: 150,
-                                  height: 125,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.all(
-                                      Radius.circular(10.0),
-                                    ),
-                                    shape: BoxShape.rectangle,
-                                    image: DecorationImage(
-                                        fit: BoxFit.fill, image: imageProvider),
-                                  ),
-                                ),
-                                progressIndicatorBuilder:
-                                    (context, url, downloadProgress) =>
-                                        CircularProgressIndicator(
-                                            value: downloadProgress.progress),
-                                errorWidget: (context, url, error) => Icon(
-                                  Icons.error,
-                                  size: 35,
-                                ),
-                                fadeOutDuration: Duration(seconds: 1),
-                                fadeInDuration: Duration(seconds: 2),
-                              ),
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
+                        )
+                      ],
+                    )
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           )
         ],
       ),
