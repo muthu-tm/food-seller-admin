119d98012f5689dbd5484a4c13e57edc3ab40bc8
madhank93 <maddymadhan946@gmail.com>
2020-10-18 09:49:58 +0530

    UI revamp of product details screen

--- lib/screens/products/ProductDetailsScreen.dart
+++ lib/screens/products/ProductDetailsScreen.dart
@@ -17,7 +17,33 @@ class ProductDetailsScreen extends StatefulWidget {
   _ProductDetailsScreenState createState() => _ProductDetailsScreenState();
 }
 
-class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+class _ProductDetailsScreenState extends State<ProductDetailsScreen>
+    with SingleTickerProviderStateMixin {
+  TabController _controller;
+
+  List<Widget> list = [
+    Tab(
+      icon: Icon(
+        Icons.card_travel,
+      ),
+      text: "Details",
+    ),
+    Tab(
+      icon: Icon(FontAwesomeIcons.comments),
+      text: "Reviews",
+    ),
+    Tab(
+      icon: Icon(FontAwesomeIcons.bookReader),
+      text: "FAQs",
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: list.length, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,101 +52,11 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
         backgroundColor: CustomColors.green,
         elevation: 0,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios, color: CustomColors.black,),
+          icon: Icon(Icons.arrow_back_ios, color: CustomColors.black),
           onPressed: () => Navigator.pop(context),
         ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.shopping_cart, color: CustomColors.black,),
-            onPressed: () {},
-          ),
-        ],
       ),
       body: getBody(context),
-      bottomNavigationBar: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height / 11,
-        child: Container(
-          padding: EdgeInsets.all(8.0),
-          decoration: BoxDecoration(
-            border: Border.all(width: 0.5, color: Colors.black12),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  SizedBox(
-                    child: Divider(
-                      color: Colors.black26,
-                      height: 4,
-                    ),
-                    height: 24,
-                  ),
-                  widget.product.offer > 0
-                      ? Text(
-                          '₹ ${widget.product.originalPrice.toString()}',
-                          style: TextStyle(
-                              color: Colors.black54,
-                              fontSize: 18,
-                              decoration: TextDecoration.lineThrough),
-                        )
-                      : Container(),
-                  SizedBox(
-                    width: 6,
-                  ),
-                  Text(
-                    '₹ ${widget.product.currentPrice.toString()}',
-                    style: TextStyle(
-                        color: Colors.red,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w700),
-                  )
-                ],
-              ),
-              SizedBox(
-                width: 6,
-              ),
-              RaisedButton(
-                onPressed: () {},
-                textColor: Colors.white,
-                padding: const EdgeInsets.all(0.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Container(
-                  width: MediaQuery.of(context).size.width / 2.9,
-                  height: 60,
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: <Color>[
-                        CustomColors.blueGreen,
-                        CustomColors.green,
-                      ],
-                    ),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(8.0),
-                    ),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Icon(
-                        Icons.add_shopping_cart,
-                        color: Colors.white,
-                      ),
-                      Text(
-                        "Buy Now",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 
@@ -162,16 +98,116 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
             Padding(
               padding: EdgeInsets.all(10.0),
               child: Container(
-                color: CustomColors.grey,
-                child: Column(
-                  children: [
-                    ListTile(
-                      leading: Icon(
-                        Icons.widgets,
-                        size: 35,
-                        color: CustomColors.blueGreen,
+                decoration: BoxDecoration(
+                  color: CustomColors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10.0),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            width: 50,
+                            height: 30,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.rectangle,
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10.0),
+                                ),
+                                color: CustomColors.white),
+                            child: Icon(
+                              Icons.update,
+                              size: 30,
+                              color: Color(0xFFAB436B),
+                            ),
+                          ),
+                          Text(
+                            widget.product.isReturnable
+                                ? "Returnable"
+                                : "Not Returnable",
+                            style: TextStyle(
+                                color: CustomColors.black,
+                                fontFamily: 'Georgia'),
+                          )
+                        ],
                       ),
-                      title: Text(
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            width: 50,
+                            height: 30,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.rectangle,
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10.0),
+                                ),
+                                color: CustomColors.white),
+                            child: Icon(
+                                widget.product.isDeliverable
+                                    ? FontAwesomeIcons.shippingFast
+                                    : Icons.transfer_within_a_station,
+                                size: 30,
+                                color: CustomColors.blue),
+                          ),
+                          Text(
+                            widget.product.isDeliverable
+                                ? "Home Delivery"
+                                : "Self Pickup",
+                            style: TextStyle(
+                                color: CustomColors.black,
+                                fontFamily: 'Georgia'),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            width: 50,
+                            height: 30,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.rectangle,
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10.0),
+                                ),
+                                color: CustomColors.white),
+                            child: Icon(
+                              widget.product.isAvailable
+                                  ? Icons.sentiment_very_satisfied
+                                  : Icons.sentiment_dissatisfied,
+                              size: 30,
+                              color: widget.product.isAvailable
+                                  ? CustomColors.green
+                                  : CustomColors.alertRed,
+                            ),
+                          ),
+                          Text(
+                            widget.product.isAvailable
+                                ? "In Stock"
+                                : "Out Of Stock",
+                            style: TextStyle(
+                                color: CustomColors.black,
+                                fontFamily: 'Georgia'),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      Text(
                         "Quantity",
                         style: TextStyle(
                           color: CustomColors.blue,
@@ -179,7 +215,7 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      trailing: Text(
+                      Text(
                         "${widget.product.weight} ${widget.product.getUnit()}",
                         textAlign: TextAlign.start,
                         style: TextStyle(
@@ -188,129 +224,60 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Column(
-                            children: <Widget>[
-                              Container(
-                                width: 50,
-                                height: 50,
-                                decoration: BoxDecoration(
-                                    shape: BoxShape.rectangle,
-                                    borderRadius: BorderRadius.all(
-                                      Radius.circular(10.0),
-                                    ),
-                                    color: CustomColors.white),
-                                child: Icon(
-                                  Icons.update,
-                                  size: 35,
-                                  color: Color(0xFFAB436B),
-                                ),
-                              ),
-                              Text(
-                                widget.product.isReturnable
-                                    ? "Returnable"
-                                    : "Not Returnable",
-                                style: TextStyle(
-                                    color: CustomColors.black,
-                                    fontFamily: 'Georgia'),
-                              )
-                            ],
-                          ),
-                          Column(
-                            children: <Widget>[
-                              Container(
-                                width: 50,
-                                height: 50,
-                                decoration: BoxDecoration(
-                                    shape: BoxShape.rectangle,
-                                    borderRadius: BorderRadius.all(
-                                      Radius.circular(10.0),
-                                    ),
-                                    color: CustomColors.white),
-                                child: Icon(
-                                    widget.product.isDeliverable
-                                        ? FontAwesomeIcons.shippingFast
-                                        : Icons.transfer_within_a_station,
-                                    size: 35,
-                                    color: CustomColors.blue),
-                              ),
-                              Text(
-                                widget.product.isDeliverable
-                                    ? "Home Delivery"
-                                    : "Self Pickup",
-                                style: TextStyle(
-                                    color: CustomColors.black,
-                                    fontFamily: 'Georgia'),
-                              )
-                            ],
-                          ),
-                          Column(
-                            children: <Widget>[
-                              Container(
-                                width: 50,
-                                height: 50,
-                                decoration: BoxDecoration(
-                                    shape: BoxShape.rectangle,
-                                    borderRadius: BorderRadius.all(
-                                      Radius.circular(10.0),
-                                    ),
-                                    color: CustomColors.white),
-                                child: Icon(
-                                  widget.product.isAvailable
-                                      ? Icons.sentiment_very_satisfied
-                                      : Icons.sentiment_dissatisfied,
-                                  size: 35,
-                                  color: widget.product.isAvailable ? CustomColors.green : CustomColors.alertRed,
-                                ),
-                              ),
-                              Text(
-                                widget.product.isAvailable
-                                    ? "In Stock"
-                                    : "Out Of Stock",
-                                style: TextStyle(
-                                    color: CustomColors.black,
-                                    fontFamily: 'Georgia'),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "-",
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  widget.product.offer > 0
+                      ? Text(
+                          '₹ ${widget.product.originalPrice.toString()}',
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 18,
+                              decoration: TextDecoration.lineThrough),
+                        )
+                      : Container(),
+                  SizedBox(
+                    width: 6,
+                  ),
+                  Text(
+                    '₹ ${widget.product.currentPrice.toString()}',
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700),
+                  )
+                ],
               ),
             ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: Container(
-                color: CustomColors.white,
-                padding: EdgeInsets.all(10),
-                alignment: Alignment.topLeft,
-                child: Column(
-                  children: [
-                    ListTile(
-                      leading: Icon(
-                        Icons.description,
-                        size: 35,
-                        color: CustomColors.blueGreen,
-                      ),
-                      title: Text(
-                        "Description",
-                        style: TextStyle(
-                            fontSize: 16,
-                            color: CustomColors.blue,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Georgia'),
-                      ),
-                    ),
-                    ListTile(
-                      leading: Text(""),
-                      title: Text(
+            SizedBox(
+              height: 20,
+            ),
+            Column(
+              children: [
+                Container(
+                  child: TabBar(
+                      indicatorColor: Colors.grey,
+                      labelColor: Colors.black,
+                      unselectedLabelColor: Colors.grey,
+                      controller: _controller,
+                      tabs: list),
+                ),
+                Container(
+                  height: 200,
+                  child: TabBarView(controller: _controller, children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
                         widget.product.shortDetails,
                         textAlign: TextAlign.start,
                         style: TextStyle(
@@ -319,10 +286,12 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                             fontFamily: 'Georgia'),
                       ),
                     ),
-                  ],
-                ),
-              ),
-            ),
+                    Container(),
+                    Container()
+                  ]),
+                )
+              ],
+            )
           ],
         ),
       ),
