58b27bf493378c27a131be332b092d8c434ce428
madhank93 <maddymadhan946@gmail.com>
2020-10-18 10:29:43 +0530

    Added toggle button to the product list widget

--- lib/screens/products/ProductWidget.dart
+++ lib/screens/products/ProductWidget.dart
@@ -6,10 +6,24 @@ import 'package:chipchop_seller/screens/utils/CustomColors.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class ProductWidget extends StatelessWidget {
+class ProductWidget extends StatefulWidget {
   ProductWidget(this.product);
 
   final Products product;
+
+  @override
+  _ProductWidgetState createState() => _ProductWidgetState();
+}
+
+class _ProductWidgetState extends State<ProductWidget> {
+  bool isSwitched;
+
+  @override
+  void initState() {
+    super.initState();
+    isSwitched = true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -21,74 +35,100 @@ class ProductWidget extends StatelessWidget {
           ),
           color: CustomColors.white,
         ),
-        child: ListTile(
+        child: InkWell(
           onTap: () {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => ProductDetailsScreen(product),
+                builder: (context) => ProductDetailsScreen(widget.product),
                 settings: RouteSettings(name: '/settings/products/view'),
               ),
             );
           },
-          leading: CachedNetworkImage(
-            imageUrl: product.getProductImage(),
-            imageBuilder: (context, imageProvider) => Container(
-              width: 60,
-              height: 70,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5.0),
-                ),
-                shape: BoxShape.rectangle,
-                image:
-                    DecorationImage(fit: BoxFit.fill, image: imageProvider),
-              ),
-            ),
-            progressIndicatorBuilder: (context, url, downloadProgress) =>
-                CircularProgressIndicator(value: downloadProgress.progress),
-            errorWidget: (context, url, error) => Icon(
-              Icons.error,
-              size: 35,
-            ),
-            fadeOutDuration: Duration(seconds: 1),
-            fadeInDuration: Duration(seconds: 2),
-          ),
-          title: Text(
-            product.name,
-            style: TextStyle(
-              fontFamily: 'Georgia',
-              color: CustomColors.blue,
-              fontSize: 14.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          subtitle: Text(
-            "${product.weight} ${product.getUnit()} - Rs. ${product.originalPrice.toString()}",
-            style: TextStyle(
-              color: CustomColors.blue,
-              fontSize: 13.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          trailing: Column(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              IconButton(
-                icon: Icon(
-                  FontAwesomeIcons.solidEdit,
-                  color: CustomColors.alertRed,
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => EditProducts(product),
-                      settings: RouteSettings(name: '/settings/products/edit'),
+              Column(
+                children: [
+                  CachedNetworkImage(
+                    imageUrl: widget.product.getProductImage(),
+                    imageBuilder: (context, imageProvider) => Container(
+                      width: 60,
+                      height: 70,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5.0),
+                        ),
+                        shape: BoxShape.rectangle,
+                        image: DecorationImage(
+                            fit: BoxFit.fill, image: imageProvider),
+                      ),
                     ),
-                  );
-                },
+                    progressIndicatorBuilder:
+                        (context, url, downloadProgress) =>
+                            CircularProgressIndicator(
+                                value: downloadProgress.progress),
+                    errorWidget: (context, url, error) => Icon(
+                      Icons.error,
+                      size: 35,
+                    ),
+                    fadeOutDuration: Duration(seconds: 1),
+                    fadeInDuration: Duration(seconds: 2),
+                  ),
+                ],
               ),
+              Column(
+                children: [
+                  Text(
+                    widget.product.name,
+                    style: TextStyle(
+                      fontFamily: 'Georgia',
+                      color: CustomColors.blue,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "${widget.product.weight} ${widget.product.getUnit()} - Rs. ${widget.product.originalPrice.toString()}",
+                    style: TextStyle(
+                      color: CustomColors.blue,
+                      fontSize: 13.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      FontAwesomeIcons.solidEdit,
+                      color: CustomColors.alertRed,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => EditProducts(widget.product),
+                          settings:
+                              RouteSettings(name: '/settings/products/edit'),
+                        ),
+                      );
+                    },
+                  ),
+                  Switch(
+                    value: isSwitched,
+                    onChanged: (value) {
+                      setState(() {
+                        isSwitched = value;
+                      });
+                    },
+                    inactiveTrackColor: Colors.red,
+                    activeTrackColor: Colors.lightGreenAccent,
+                    activeColor: Colors.green,
+                  ),
+                ],
+              )
             ],
           ),
         ),
