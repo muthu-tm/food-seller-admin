4dfe88442d68a82ab2d9becc125c48f9c620dfb5
Muthu Thavamani <muthucs@outlook.com>
2020-11-03 08:43:45 +0530

    chat notification flag issue fixed

--- lib/screens/app/CustomersBottomWidget.dart
+++ lib/screens/app/CustomersBottomWidget.dart
@@ -27,39 +27,8 @@ class _CustomersBottomWidgetState extends State<CustomersBottomWidget> {
     _newStoreNotification = newStoreNotification;
     _firebaseMessaging.configure(
       onMessage: (Map<String, dynamic> message) async {
-        print("onMessage: $message");
         if (message['data']['type'] == '1') {
-          // showDialog(
-          //   context: context,
-          //   builder: (context) => AlertDialog(
-          //     content: ListTile(
-          //       title: Text(
-          //         message['notification']['title'],
-          //         style: TextStyle(
-          //             color: CustomColors.green,
-          //             fontSize: 16.0,
-          //
-          //             fontWeight: FontWeight.bold),
-          //         textAlign: TextAlign.start,
-          //       ),
-          //       subtitle: Text(
-          //         message['notification']['body'],
-          //         style: TextStyle(
-          //             fontSize: 14.0,
-          //
-          //             fontWeight: FontWeight.bold),
-          //       ),
-          //     ),
-          //     actions: <Widget>[
-          //       FlatButton(
-          //         child: Text('OK'),
-          //         onPressed: () => Navigator.of(context).pop(),
-          //       ),
-          //     ],
-          //   ),
-          // );
-
-          await ChatTemplate().updateToUnRead(
+          ChatTemplate().updateToUnRead(
             message['data']['store_uuid'],
             message['data']['customer_uuid'],
           );
@@ -107,77 +76,78 @@ class _CustomersBottomWidgetState extends State<CustomersBottomWidget> {
     _newStoreNotification
         ? child = SizedBox.fromSize(
             size: widget.size,
-            child: Stack(
-              alignment: Alignment.center,
-              children: <Widget>[
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => CustomersHome(),
-                        settings: RouteSettings(name: '/customers'),
-                      ),
-                    ).then((value) {
-                      newStoreNotification = false;
-                      _newStoreNotification = false;
-                    });
-                  },
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(
-                        Icons.group,
-                        size: 30.0,
-                        color: CustomColors.black,
-                      ),
-                      Text("Customers",
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: GoogleFonts.orienta()),
-                    ],
-                  ),
-                ),
-                Positioned(
-                  right: 25,
-                  top: 10,
-                  child: Container(
-                    padding: EdgeInsets.all(1),
-                    decoration: BoxDecoration(
-                      color: CustomColors.alertRed,
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    constraints: BoxConstraints(
-                      minWidth: 13,
-                      minHeight: 13,
-                    ),
-                    child: Text(
-                      '',
-                      style: TextStyle(
-                        color: CustomColors.black,
-                        fontSize: 8,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                )
-              ],
-            ),
-          )
-        : child = SizedBox.fromSize(
-            size: widget.size,
             child: InkWell(
               onTap: () {
+                setState(() {
+                  _newStoreNotification = false;
+                  newStoreNotification = false;
+                });
+
                 Navigator.push(
                   context,
                   MaterialPageRoute(
                     builder: (context) => CustomersHome(),
                     settings: RouteSettings(name: '/customers'),
                   ),
-                ).then((value) {
-                  newStoreNotification = false;
+                );
+              },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Stack(alignment: Alignment.center, children: <Widget>[
+                    Icon(
+                      Icons.group,
+                      size: 30.0,
+                      color: CustomColors.black,
+                    ),
+                    Positioned(
+                      right: 0,
+                      top: 0,
+                      child: Container(
+                        padding: EdgeInsets.all(1),
+                        decoration: BoxDecoration(
+                          color: CustomColors.alertRed,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        constraints: BoxConstraints(
+                          minWidth: 12,
+                          minHeight: 12,
+                        ),
+                        child: Text(
+                          '',
+                          style: TextStyle(
+                            color: CustomColors.black,
+                            fontSize: 8,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ]),
+                  Text("Customers",
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: GoogleFonts.orienta()),
+                ],
+              ),
+            ),
+          )
+        : child = SizedBox.fromSize(
+            size: widget.size,
+            child: InkWell(
+              onTap: () {
+                setState(() {
                   _newStoreNotification = false;
+                  newStoreNotification = false;
                 });
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CustomersHome(),
+                    settings: RouteSettings(name: '/customers'),
+                  ),
+                );
               },
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
