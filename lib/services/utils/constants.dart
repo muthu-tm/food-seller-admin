9d3132124593adc9e079e01e815c33901770ced2
Muthu Thavamani <muthucs@outlook.com>
2020-12-10 21:40:54 +0530

    uniques logo instead no image placeholder

--- lib/services/utils/constants.dart
+++ lib/services/utils/constants.dart
@@ -11,7 +11,7 @@ const seller_profile_folder = "seller_profile";
 const store_offers_folder = "store_offers";
 
 const no_image_placeholder =
-    "https://firebasestorage.googleapis.com/v0/b/fc-chipchop-test.appspot.com/o/app_images%2Fno-image-placeholder.png?alt=media&token=c2ff5e97-d21d-4137-8d25-b0fc380f6f8f";
+    "https://firebasestorage.googleapis.com/v0/b/fc-chipchop-test.appspot.com/o/app_images%2Funiques_logo.png?alt=media&token=7f16bde8-c3a6-4a51-9715-a41e6ee118c9";
 
 const seller_app_name = "Uniques Seller";
 const app_version = '1.0.0';
